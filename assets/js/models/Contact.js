3645326ba1520498c7c0a0da8ec9a7598b9288fa
Lucie99999 <lucie.saurel3@orange.fr>
2024-07-26 17:56:57 +0200

    feat(Contact):create contact class

--- assets/js/models/Contact.js
+++ assets/js/models/Contact.js
@@ -7,5 +7,10 @@
  */
 
 export class Contact {
-    constructor (nom, prenom, telephone);
+    constructor (nom, prenom, telephone){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+    }
 }
+
